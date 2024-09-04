d8d55d58238d8e5e9de719f2b61a6ebb4379bd0b
Laju979 <lajuaroeye@gmail.com>
2024-09-04 11:09:36 +0100

    Add files via upload

---
+++ MyHomepage.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.sidebar.header('Project Designed')
+st.sidebar.markdown("[Project designed: Nigeria Knowledge Chatbot](https://askaboutnigeria.streamlit.app/)")
+st.sidebar.markdown("[Project designed: Diabetes Diagnosis](https://diabetes.streamlit.app/)")
+st.sidebar.markdown("[Project design: Salary Prediction](http://salarylinear.streamlit.app/)")
+st.sidebar.markdown("[Project design: BMI Indicator](https://patientbmi.streamlit.app/)")
+st.sidebar.markdown("[Project design: Bikes and Bicycles](https://teachablemachine.withgoogle.com/models/wz3JTR2Vq/)")
+
+st.header('Lajus Homepage')
+st.subheader('Senior Machine Learning Engineer')
+st.write('My Experience'
+         'For the 6 long weeks I have spent here at Rolof Computer Academy, I, along with several other course mates underwent several hours of training which in the end led to my newly found love for coding. It really was a wonderful experience at the Rolof A.I Bootcamp.')
